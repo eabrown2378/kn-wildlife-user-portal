4bf18d5a747d042ee9d5cfdbcecf9b95b7331980
eabrown2378 <59630661+eabrown2378@users.noreply.github.com>
2025-04-30 15:18:43 -0400

    production ready w/ ssl cert

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   server: {
-    https: {
-      cert: "/home/ebrown23/kn-wildlife_crc_nd_edu.cer",
-      key: "/home/ebrown23/kn-wildlife.crc.nd.edu.key"
-    }
+    https: true,
+    cors: true
   }
 });
